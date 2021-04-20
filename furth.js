9729009fe71ab30608f9373c4a4318ef6c4acaee
shreefaw <shreefaw@gmail.com>
2021-04-20 20:55:52 +0300

    first commit

---
+++ furth.js
@@ -0,0 +1,22 @@
+let value = prompt("Your color");
+
+switch(value){
+  case "red": 
+    alert ("apple");
+    break
+    case "yellow":
+    alert ("banana");
+    break
+    case "green":
+    alert("grass");
+    break
+    case "circle":
+    case "rectangle":
+    case "square":
+    alert ("shape");
+    break
+  default:
+  alert("i don't know");
+  break 
+    
+}
---
+++ furthPart2.js
@@ -0,0 +1,14 @@
+let value = prompt("Your color");
+
+if(value == "red"){
+    alert ("apple");}
+else if(value =="yellow"){
+    alert ("banana");}
+else if(value == "green"){
+    alert("grass");}
+else if(value == "circle"|| value == "rectangle" || value == "sqaure"){
+    alert ("shape");}
+else{
+  alert("i don't know")}
+  
+    
